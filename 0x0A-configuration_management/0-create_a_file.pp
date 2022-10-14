4b147d86ba1637d976b6ab5c440f3583e1b753ce
cmmasaba <mmasaba3142@gmail.com>
2022-10-14 11:33:49 +0300

    Create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# a manifest that creates a file in /tmp directory
+
+file { '/tmp/school':
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
